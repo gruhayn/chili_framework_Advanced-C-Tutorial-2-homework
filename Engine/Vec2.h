9344476f395eafbcc501019d485da143142c5599
Natig Aghayev <aganatiq98@gmail.com>
2022-05-08 13:34:41 +0300

    vec2 added

---
+++ Engine/Vec2.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cmath>
+
+template <typename T>
+class Vec2 {
+
+public:
+	explicit Vec2(T x, T y) :
+		x(x),
+		y(y)
+	{};
+
+	Vec2<T> operator+(const Vec2<T>& rhs) {
+		return Vec2<T>{x + rhs.x, y + rhs.y};
+	}
+
+	Vec2<T>& operator+=(const Vec2<T>& rhs) {
+		return *this = *this + rhs;;
+	}
+	
+	Vec2<T> operator*(const T& rhs) {
+		return Vec2<T>{x * rhs, y * rhs};
+	}
+
+	Vec2<T>& operator*=(const T& rhs) {
+		return *this = *this * rhs;
+	}
+
+	T GetLength() {
+		return x * x + y * y;
+	}
+
+	float GetLengthNormalized() {
+		return sqrt((float)GetLength());
+	}
+
+public:
+	T x;
+	T y;
+};
